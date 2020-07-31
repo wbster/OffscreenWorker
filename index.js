2f92dd46a24ad0b83646c4cce82534db7918b553
max <wencorsman@gmail.com>
2020-07-31 03:11:38 +0300

    addded jsdoc

--- index.js
+++ index.js
@@ -3,38 +3,76 @@ import Axios from 'axios'
 
 export class OffscreenWorker extends EventEmitter {
 
-    constructor(link) {
+    /**
+     * 
+     * @param {string} link 
+     * @param {boolean | undefined} [customCheckWorkerMode] 
+     */
+    constructor(link, customCheckWorkerMode) {
         super()
-        if (!!self.OffscreenCanvas) {
-            console.log('origin offscreen')
-            const worker = new Worker(link)
-            // @ts-ignore
-            return worker
-        }
         this.ready = false
-        this.on('message', data => {
-            if (!this.ready) {
-                setTimeout(() => this.emit('message', data), 10)
-            }
+        this.once('ready', () => this.ready = true)
+        this.init(link, customCheckWorkerMode)
+    }
+
+    /**
+     * @param {string} link 
+     */
+    init(link, customCheckWorkerMode) {
+        if (
+            customCheckWorkerMode !== undefined && customCheckWorkerMode ||
+            customCheckWorkerMode === undefined && !!self.OffscreenCanvas
+        ) {
+            this.initWorker(link)
+        } else {
+            this.initFakeWorker(link)
+        }
+    }
+
+    /**
+     * @param {string} link 
+     */
+    initFakeWorker(link) {
+        this.ready = false
+        this.on('post', (data, opt) => {
+            if (!this.ready) this.once('ready', () => this.emit('post', data, opt))
+            else this.emit('message', data, opt)
         })
-        console.log('fake offscreen')
         Axios
             .get(link)
             .then(r => r.data)
             .then(code => {
                 const self = this
                 eval(code)
-                this.ready = true
+                this.emit('ready')
                 return self
             })
     }
 
+    /**
+     * @param {string} link 
+     */
+    initWorker(link) {
+        const worker = new Worker(link)
+        worker.addEventListener('message', ({ data }) => this.emit('message', data))
+        this.on('post', (data, opt) => worker.postMessage(data, opt))
+    }
+
+    /**
+     * @param {string} name 
+     * @param {function(...any):void} func 
+     */
     addEventListener(name, func) {
         this.on(name, func)
     }
 
+    /**
+     * 
+     * @param {Object} data 
+     * @param {object} [opt] 
+     */
     postMessage(data, opt) {
-        this.emit('message', ({ data }))
+        this.emit('post', data, opt)
     }
 
 }
